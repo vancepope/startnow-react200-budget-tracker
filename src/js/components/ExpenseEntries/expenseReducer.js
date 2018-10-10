e6f1245e039e494641a2f51f61cfbc7f644032aa
Vance Pope <vancepope@outlook.com>
2018-10-09 18:45:26 -0700

    added reducers - 10/09/2018

---
+++ src/js/components/ExpenseEntries/expenseReducer.js
@@ -0,0 +1,9 @@
+const defaultState = {
+  description: '',
+  amount: '',
+  lineItems: []
+};
+
+export default function ExpenseReducer (state = defaultState, action) {
+  return state;
+}
---
+++ src/js/components/IncomeEntries/incomeReducer.js
@@ -0,0 +1,9 @@
+const defaultState = {
+  description: '',
+  amount: '',
+  lineItems: []
+};
+
+export default function IncomeReducer (state = defaultState, action) {
+  return state;
+}
