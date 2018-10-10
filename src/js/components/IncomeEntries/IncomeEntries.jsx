c8728db9d63fa9d204677aded478d39c917cc2cf
Vance Pope <vancepope@outlook.com>
2018-10-09 20:09:14 -0700

    implemented actions - 10/09/2018

--- src/js/components/IncomeEntries/IncomeEntries.jsx
+++ src/js/components/IncomeEntries/IncomeEntries.jsx
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
 
+import {
+  updateIncomeDescription,
+  updateIncomeAmount,
+  addIncome
+} from './incomeActions';
+
 export default class IncomeEntries extends Component {
   constructor(props) {
     super(props);
+
+    this.handleDescriptionInput = this.handleDescriptionInput.bind(this);
+    this.handleAmountInput = this.handleAmountInput.bind(this);
+    this.handleAddIncome = this.handleAddIncome.bind(this);
+  }
+
+  handleDescriptionInput(event) {
+    const { dispatch } = this.props;
+    const { value } = event.target;
+    dispatch(updateIncomeDescription(value));
+  }
+
+  handleAmountInput(event) {
+    const { dispatch } = this.props;
+    const { value } = event.target;
+    dispatch(updateIncomeAmount(value));
+  }
+
+  handleAddIncome() {
+    const { description, amount, dispatch } = this.props;
+    dispatch(addIncome(description, amount));
   }
 
   render() {
+    const { description, amount, lineItems } = this.props;
     return (
       <div className='card border-success mb-3'>
-        <div className='card-header text-white bg-success'>Income Entries</div>
+        <div className='card-header text-white bg-success'>Expense Entries</div>
         <div className='card-body'>
           <form>
             <div className='form-group'>
@@ -17,6 +45,8 @@ export default class IncomeEntries extends Component {
                 type='text'
                 className='form-control'
                 id='income-description'
+                value={ description }
+                onChange={ this.handleDescriptionInput }
               />
             </div>
             <div className='form-group'>
@@ -27,12 +57,15 @@ export default class IncomeEntries extends Component {
                   type='text'
                   className='form-control'
                   id='income-amount'
+                  value={ amount }
+                  onChange={ this.handleAmountInput }
                 />
               </div>
             </div>
             <button
               type='button'
               className='btn btn-success col-12 mb-5'
+              onClick={ this.handleAddIncome }
             >+ Add Income
             </button>
             <table className='table table-sm table-hover'>
@@ -43,10 +76,14 @@ export default class IncomeEntries extends Component {
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td>Paycheck</td>
-                  <td>$1,500.00</td>
-                </tr>
+                {
+                  lineItems.map(lineItem => (
+                    <tr>
+                      <td>{ lineItem.description }</td>
+                      <td>${ lineItem.amount.toFixed(2) }</td>
+                    </tr>
+                  ))
+                }
               </tbody>
             </table>
           </form>
--- src/js/components/IncomeEntries/incomeReducer.js
+++ src/js/components/IncomeEntries/incomeReducer.js
@@ -4,6 +4,38 @@ const defaultState = {
   lineItems: []
 };
 
-export default function IncomeReducer (state = defaultState, action) {
-  return state;
+export default function IncomeReducer(state = defaultState, action) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case 'UPDATE_INCOME_DESCRIPTION': {
+      return {
+        ...state,
+        description: payload.description
+      };
+    }
+
+    case 'UPDATE_INCOME_AMOUNT': {
+      return {
+        ...state,
+        amount: payload.amount
+      };
+    }
+
+    case 'ADD_INCOME': {
+      const { description, amount } = action.payload;
+      return {
+        description: '',
+        action: '',
+        lineItems: [
+          ...state.lineItems,
+          { description, amount }
+        ]
+      };
+    }
+
+    default: {
+      return state;
+    }
+  }
 }
